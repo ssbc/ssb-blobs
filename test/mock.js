d881abbb36d53c1992f167daf769864bf060f1c3
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-17 14:10:51 +1200

    test for races with interleavings

--- test/mock.js
+++ test/mock.js
@@ -7,7 +7,8 @@ function hash(buf) {
             .update(buf).digest('base64')+'.sha256'
 }
 
-module.exports = function MockBlobStore () {
+module.exports = function MockBlobStore (name, async) {
+
   var store = {}
   function add (buf, _h) {
     var h = hash(buf)
@@ -18,26 +19,30 @@ module.exports = function MockBlobStore () {
 
   return {
     store: store,
+
     get: function (blobId) {
+      console.log('GET', !!store[blobId], blobId)
       if(!store[blobId])
-        return pull.error(new Error('no blob:'+blobId))
-      return pull.values([store[blobId]])
+        return pull(pull.error(new Error('no blob:'+blobId)), async.through('get-error'))
+      return pull(pull.values([store[blobId]]), async.through('get'))
     },
-    has: function (blobId, cb) {
-      cb(null, store[blobId] ? true : false)
-    },
-    size: function (blobId, cb) {
-      cb(null, store[blobId] ? store[blobId].length : null)
-    },
+    has: async(function (blobId, cb) {
+      console.log('**** HAS', name, blobId)
+      async(cb, 'has-cb')(null, store[blobId] ? true : false)
+    }, 'has'),
+    size: async(function (blobId, cb) {
+      console.log('**** SIZE', name, blobId)
+      async(cb, 'size-cb')(null, store[blobId] ? store[blobId].length : null)
+    }, 'size'),
     add: function (_hash, cb) {
       if('function' == typeof _hash)
         cb = _hash, _hash = null
-      return pull.collect(function (err, data) {
+      return pull(async.through('add'), pull.collect(async(function (err, data) {
         if(err) return cb(err)
         var h = add(Buffer.concat(data), _hash)
         if(!h) cb(new Error('wrong hash'))
         else cb(null, h)
-      })
+      }, 'add-cb')))
     }
   }
 }
