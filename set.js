936e68972c05baca82106ecd626936f02d101fac
Dominic Tarr <dominic.tarr@gmail.com>
2016-06-09 15:44:13 +1200

    make Set use correct values

--- set.js
+++ set.js
@@ -10,18 +10,19 @@ module.exports = function (db) {
       if(!e.sync)
       if(e.type === 'del')
         delete set[e.key]
-      else set[e.key] = true
+      else set[e.key] = e.value
     })
   )
 
   return {
     set: set,
     add: function (key, cb) {
-      db.put(key, 0, cb)
+      db.put(key, -1, cb)
     },
     remove: function (key, cb) {
-      db.del(key, 0, cb)
+      db.del(key, cb)
     }
   }
 }
+
 
