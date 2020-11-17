b6d4f710f69f86218984425cf4271621f3de3af6
mixmix <whimful@gmail.com>
2020-11-17 14:05:51 +1300

    replace secret-stack with scuttle-testbot, add tap-spec

--- test/secret-stack.js
+++ test/secret-stack.js
@@ -1,48 +1,36 @@
-var SecretStack = require('secret-stack')
-var crypto      = require('crypto')
-var tape        = require('tape')
-var path        = require('path')
-var osenv       = require('osenv')
-var mkdirp      = require('mkdirp')
-var pull        = require('pull-stream')
+var tape = require('tape')
+var pull = require('pull-stream')
+const { hash, Server } = require('./util')
 
-//deterministic keys make testing easy.
-function hash (s) {
-  return crypto.createHash('sha256').update(s).digest()
-}
-
-var appkey = hash('TESTBLOBS')
-
-var create = SecretStack({ caps: {shs: appkey} })
-  .use(require('../'))
-
-function tmp (name) {
-  var dir = path.join(osenv.tmpdir(), 'testblobs-'+Date.now()+'-'+name)
-  mkdirp.sync(dir)
-  return dir
+// deterministic keys make testing easy.
+const caps = {
+  shs: hash('TESTBLOBS')
 }
 
 tape('alice pushes to bob', function (t) {
-  var alice = create({ seed: hash('ALICE'), path: tmp('alice') })
-  var bob = create({ seed: hash('BOB'), path: tmp('bob') })
+  var alice = Server({ seed: hash('ALICE'), caps })
+  var bob = Server({ seed: hash('BOB'), caps })
 
   // Avoid race because of async server creation, introduced secret-stack@6.
   //
   // This `once()` function must be run before the event is emit, which is why
-  // `create()` must be run directly above. If `once()` is called much later
-  // than `create()` then the event is emit before the listener is created.
+  // `Server()` must be run directly above. If `once()` is called much later
+  // than `Server()` then the event is emit before the listener is created.
   bob.once('multiserver:listening', function () {
     alice.connect(bob.address(), function (err, rpc) {
-      if(err) throw err
+      if (err) throw err
     })
 
-    var hello = new Buffer('Hello World'), _hash
+    var hello = Buffer.from('Hello World')
+    var _hash
 
     pull(
-      bob.blobs.ls({live: true, long: true}),
+      bob.blobs.ls({ live: true, long: true }),
       pull.filter((msg) => !msg.sync),
       pull.take(1),
       pull.collect(function (err, ary) {
+        if (err) throw err
+
         t.equal(ary[0].id, _hash)
         t.end()
         alice.close()
@@ -53,6 +41,7 @@ tape('alice pushes to bob', function (t) {
     pull(
       pull.values([hello]),
       alice.blobs.add(function (err, hash) {
+        if (err) throw err
         _hash = hash
         alice.blobs.push(hash)
       })
@@ -61,13 +50,12 @@ tape('alice pushes to bob', function (t) {
 })
 
 tape('close', t => {
-  var path = tmp('alice')
-  var alice = create({ seed: hash('ALICE'), path })
+  var alice = Server({ seed: hash('ALICE') })
 
   alice.close((err) => {
     t.error(err)
 
-    var alice = create({ seed: hash('ALICE'), path })
+    var alice = Server({ seed: hash('ALICE'), startUnclean: true })
     alice.close(t.end)
   })
 })
--- test/util.js
+++ test/util.js
@@ -2,50 +2,46 @@ const debug = require('debug')('ssb-blobs')
 var pull = require('pull-stream')
 var crypto = require('crypto')
 
-var log = exports.log = function log (name) {
-  if(process.env.DEBUG)
-    return pull.through(function (e) {
-      debug(name, e)
-    })
-  else
-    return pull.through()
+exports.log = function log (name) {
+  if (!process.env.DEBUG) return pull.through()
+
+  return pull.through(function (e) {
+    debug(name, e)
+  })
 }
 
-function bindAll(obj, context) {
+function bindAll (obj, context) {
   var o = {}
-  for(var k in obj) {
-    if(obj[k])
-      o[k] = obj[k].bind(context)
+  for (var k in obj) {
+    if (obj[k]) o[k] = obj[k].bind(context)
   }
   return o
 }
 
 exports.peers = function (nameA, a, nameB, b, async) {
-  var na = nameA[0].toUpperCase(), nb = nameB[0].toUpperCase()
-  //this is just a hack to fake RPC. over rpc each method is called
+  var na = nameA[0].toUpperCase()
+  var nb = nameB[0].toUpperCase()
+  //this is just a hack to fake rpc. over rpc each method is called
   //with the remote id in the current this context.
   a._onConnect({id: nameB, blobs: bindAll(b, {id: nameA})}, nb+na)
   b._onConnect({id: nameA, blobs: bindAll(a, {id: nameB})}, na+nb)
 }
 
-
 exports.hash = function (buf) {
-  buf = 'string' == typeof buf ? new Buffer(buf) : buf
+  buf = 'string' === typeof buf ? Buffer.from(buf) : buf
   return '&'+crypto.createHash('sha256')
             .update(buf).digest('base64')+'.sha256'
 }
 
 exports.fake = function (string, length) {
-  var b = new Buffer(length)
+  var b = Buffer.alloc(length)
   var n = Buffer.byteLength(string)
-  for(var i = 0; i < length; i += n)
-    b.write(string, i)
+  for (var i = 0; i < length; i += n) b.write(string, i)
   return b
 }
 
-
 exports.sync = function noAsync (test, done) {
-  function async(fn) {
+  function async (fn) {
     return fn
   }
   async.through = function () { return pull.through() }
@@ -63,4 +59,9 @@ exports.tests = function (tests) {
   }, exports.sync)
 }
 
+exports.Server = function Server (opts = {}) {
+  const stack = require('scuttle-testbot')
+    .use(require('..'))
 
+  return stack(opts)
+}
